aa0b802c4d218daa03419adc3ff026d5182d8a03
Tomasz Krzywicki <t-krzywicki@wp.pl>
2019-02-23 20:14:11 +0100

    added supporting and simple validation for form

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        about
+        <h2>What is it?</h2>
+
+        <p>It's simple application written in vue.js for demonstrating capabilities of this framework.
+            This app calculates network and broadcast adresses and count of hosts, base on an ip address
+            from the network and network mask.</p>
     </div>
 </template>
 
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
-        <form>
+        <form @submit="checkForm">
             <div class="form-group">
                 <label for="ip">IP address</label>
-                <input type="text" class="form-control" id="ip" placeholder="Enter IP address">
-                <small id="ip-help" class="form-text text-muted">Provide the IP address.</small>
+                <input type="text" class="form-control" id="ip" placeholder="Enter IP address"
+                       @input="ip = $event.target.value">
+                <small v-if="errors.find(x => x.field === 'ip')" id="ip-help" class="form-text text-danger">
+                    Provide the IP address.
+                </small>
             </div>
 
             <div class="form-group">
                 <label for="mask">Mask</label>
-                <input type="number" class="form-control" id="mask" placeholder="Enter network mask" min="1" max="255">
-                <small id="mask-help" class="form-text text-muted">Provide the network mask.</small>
+                <input type="number" class="form-control" id="mask" placeholder="Enter network mask"
+                       @input="mask = $event.target.value" min="1" max="255">
+                <small v-if="errors.find(x => x.field === 'mask')" id="mask-help" class="form-text text-danger">
+                    Provide the network mask.
+                </small>
             </div>
 
             <button type="submit" class="btn btn-primary">Calc</button>
@@ -19,27 +25,61 @@
         <hr>
         <div class="form-group">
             <label for="net-address">Network address</label>
-            <input type="text" class="form-control" id="net-address" disabled>
+            <input type="text" class="form-control" id="net-address" v-model="netAddress" disabled>
         </div>
 
         <div class="form-group">
             <label for="broadcast-address">Broadcast address</label>
-            <input type="text" class="form-control" id="broadcast-address" disabled>
+            <input type="text" class="form-control" id="broadcast-address" v-model="broadcastAddress" disabled>
         </div>
 
         <div class="form-group">
             <label for="host-count">Count of hosts in this network</label>
-            <input type="text" class="form-control" id="host-count" disabled>
+            <input type="text" class="form-control" id="host-count" v-model="countOfHosts" disabled>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Calculator"
+        name: "Calculator",
+        data() {
+            return {
+                errors: [],
+                ip: '',
+                mask: 0,
+                netAddress: null,
+                broadcastAddress: null,
+                countOfHosts: null
+            }
+        },
+        methods: {
+            checkForm(e) {
+                if (this.ip && this.mask) {
+                    this.getScores();
+                    return true;
+                }
+
+                if (this.ip === '') {
+                    this.errors.push({
+                        field: 'ip',
+                        text: 'IP address is required'})
+                }
+
+                if (this.mask === 0) {
+                    this.errors.push({
+                        field: 'mask',
+                        text: 'Mask is required'
+                    })
+                }
+
+                e.preventDefault();
+            },
+            getScores() {
+                this.netAddress = '192.168.1.0';
+                this.broadcastAddress = '192.168.1.255';
+                this.countOfHosts = 254;
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
