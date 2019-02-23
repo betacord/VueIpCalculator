0396df1ff1a2a247425e8b0da34cb4e17c0e50a5
Tomasz Krzywicki <t-krzywicki@wp.pl>
2019-02-23 18:50:55 +0100

    added supporting for active class in menu

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <p class="navbar-brand">Kalkulator IP</p>
+      <a class="navbar-brand">Kalkulator IP</a>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link to="/calculator" class="nav-link">Calculator <span class="sr-only">(current)</span></router-link>
+          <li class="nav-item" v-bind:class="{ active: activeLink === 0 }" @click="activeLink = 0">
+            <router-link to="/calculator" class="nav-link">Calculator</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item" v-bind:class="{ active: activeLink === 1 }" @click="activeLink = 1">
             <router-link to="/about" class="nav-link">About</router-link>
           </li>
         </ul>
       </div>
     </nav>
 
-    <div class="container">
+    <div class="container py-3">
       <router-view></router-view>
-    </div>
-  </div>
 
+      <hr>
+      <footer>
+        <p>&copy; TK 2019</p>
+      </footer>
+    </div>
+
+  </div>
 </template>
 
 
@@ -27,6 +31,11 @@
 
 export default {
   name: 'app',
+  data() {
+    return {
+      activeLink: 0
+    }
+  }
 }
 
 </script>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,36 @@
 <template>
     <div>
-        calc
+        <form>
+            <div class="form-group">
+                <label for="ip">IP address</label>
+                <input type="text" class="form-control" id="ip" placeholder="Enter IP address">
+                <small id="ip-help" class="form-text text-muted">Provide the IP address.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="mask">Mask</label>
+                <input type="number" class="form-control" id="mask" placeholder="Enter network mask" min="1" max="255">
+                <small id="mask-help" class="form-text text-muted">Provide the network mask.</small>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Calc</button>
+        </form>
+
+        <hr>
+        <div class="form-group">
+            <label for="net-address">Network address</label>
+            <input type="text" class="form-control" id="net-address" disabled>
+        </div>
+
+        <div class="form-group">
+            <label for="broadcast-address">Broadcast address</label>
+            <input type="text" class="form-control" id="broadcast-address" disabled>
+        </div>
+
+        <div class="form-group">
+            <label for="host-count">Count of hosts in this network</label>
+            <input type="text" class="form-control" id="host-count" disabled>
+        </div>
     </div>
 </template>
 
